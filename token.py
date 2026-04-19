937c0e681933ee5e78c17ae5d6f1754129c0b8aa
Map-Sol <useris6631@gmail.com>
2026-04-19 16:30:42 +0530

    Update token.py

--- token.py
+++ token.py
@@ -1,9 +1,13 @@
-import re
+text=input("enter the text for tokenisation:")
+tokens=[]
+word=""
+for ch in text:
+    if ch!=" ":
+        word+=ch
+    else:
+        tokens.append(word)
+        word=""
+tokens.append(word)
 
-text=input("enter the text for tokenisation")
-word=re.findall(r'\b\w+\b',text)
-sentence=re.split(r'[.!?]+',text)
-sentence=[s.strip() for s in sentence if s.strip()]
-print(sentence)
-print("\n")
-print(word)
+print("Original Text",text)
+print("Tokens",tokens)
