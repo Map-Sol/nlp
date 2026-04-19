378d68e6b02d1f615f4699859b90e61c8383eaf3
Map-Sol <useris6631@gmail.com>
2026-04-19 16:50:55 +0530

    Update porter.py

--- porter.py
+++ porter.py
@@ -1,21 +1,35 @@
-def simple_porter_stem(word):
-    word = word.lower()
-    if word.startswith("re"):
-        word = word[2:]
-    elif word.startswith("un"):
-        word = word[2:]
+def stem(word):
+  
     if word.endswith("sses"):
         word = word[:-2]
     elif word.endswith("ies"):
         word = word[:-2]
     elif word.endswith("s") and not word.endswith("ss"):
         word = word[:-1]
-    elif word.endswith("ing"):
+
+  
+    if word.endswith("ing"):
         word = word[:-3]
     elif word.endswith("ed"):
         word = word[:-2]
+
+
+    if word.endswith("ational"):
+        word = word[:-7] + "ate"
+    elif word.endswith("tional"):
+        word = word[:-6] + "tion"
+
+ 
+    if word.endswith("e"):
+        word = word[:-1]
+
     return word
 
-words = ["redoing", "unwanted", "caresses", "running", "cats"]
+
+
+words = ["playing", "connected", "relational", "studies"]
+word = input("Enter a word: ")
+print(word, "->", stem(word))
+
 for w in words:
-    print(w, "->", simple_porter_stem(w))
+    print(w, "->", stem(w))
