6e42178d21a38b67d2cfa53ad2fb683a686b90db
Map-Sol <useris6631@gmail.com>
2026-04-19 21:41:15 +0530

    Update sr.py

--- sr.py
+++ sr.py
@@ -3,31 +3,19 @@ from nltk import CFG
 from nltk.parse import ShiftReduceParser
 
 g = CFG.fromstring("""
-S -> NP VP
+S  -> NP VP
 NP -> DT NN
 VP -> V NP
 DT -> 'the'
 NN -> 'boy' | 'ball'
-V -> 'hit'
+V  -> 'hit'
 """)
 
-p = ShiftReduceParser(g)
+p = ShiftReduceParser(g, trace=2)
+
 s = "the boy hit the ball".split()
 
-print("{:<5} {:<30} {:<30} {:<10}".format("steps","Stack", "Input", "Action"))
-print("-"*80)
 
-stk = []
-inp = s[:]
-steps=1
-
-for w in s:
-    stk.append(w)
-    inp.pop(0)
-    print("{:<5} {:<30} {:<30} {:<10}".format(steps," ".join(stk), " ".join(inp), "SHIFT"))
-    steps+=1
-    
-print("{:<5} {:<30} {:<30} {:<10}".format(steps," ".join(stk), " ".join(inp), "reduce"))
-
-for t in p.parse(s):
-    t.pretty_print()
+for tree in p.parse(s):
+    print("\nFinal Parse Tree:\n")
+    tree.pretty_print()
