1231391640eb181b68879bd5885adf774cc9a76d
Map-Sol <useris6631@gmail.com>
2026-04-19 21:00:55 +0530

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -2,20 +2,19 @@ import nltk
 from nltk import CFG
 from nltk.parse import ChartParser
 
+
 grammar = CFG.fromstring("""
 S -> NP VP
-NP -> D N | D N PP | 'ravi' | 'raju'
-VP -> V NP | V NP PP | V PP
-PP -> P NP
-D -> 'the' | 'my'
-P -> 'in' | 'with'
-N -> 'raju' | 'ravi'
-V -> 'playing'
+NP -> Det N
+VP -> V NP
+Det -> 'The' | 'an'
+N -> 'boy' | 'apple'
+V -> 'eats'
 """)
 
 parser = ChartParser(grammar)
 
-sentence = "ravi playing with the raju".split()
+sentence = "The boy eats an apple".split()
 
 for tree in parser.parse(sentence):
     print(tree)
