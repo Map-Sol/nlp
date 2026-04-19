38656eb94038416be7860bd5c0557dd2d89ed078
Map-Sol <useris6631@gmail.com>
2026-04-19 18:25:22 +0530

    Update sentence.py

--- sentence.py
+++ sentence.py
@@ -1,13 +1,39 @@
 from sklearn.linear_model import LogisticRegression
 
-m=LogisticRegression().fit([[1],[0]],[1,0])
-t="dr. Wow! Is this working? Yes it is.".split()
-o=[t[0]]
+# Training data (slightly improved)
+X_train = [
+    [1], [1], [1],   # boundary cases
+    [0], [0], [0]    # non-boundary
+]
+y_train = [1, 1, 1, 0, 0, 0]
 
-for i in range(1,len(t)):
-    if t[i-1][-1] in ".!?" and m.predict([[t[i][0].isupper()]])[0]:
-        o.append(t[i])
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+print("Model trained successfully\n")
+
+text = input("Enter text: ")
+words = text.split()
+
+sentences = [words[0]]
+
+for i in range(1, len(words)):
+    prev_word = words[i-1]
+    curr_word = words[i]
+
+    # Feature extraction
+    has_punct = prev_word[-1] in ".!?"
+    is_capital = curr_word[0].isupper()
+
+    feature = 1 if (has_punct and is_capital) else 0
+
+    prediction = model.predict([[feature]])[0]
+
+    if prediction == 1:
+        sentences.append(curr_word)
     else:
-        o[-1]+=" "+t[i]
+        sentences[-1] += " " + curr_word
 
-print(o)
+print("\nPredicted Sentences:")
+for s in sentences:
+    print(s)
