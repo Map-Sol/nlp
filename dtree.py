2079c33663b823ca0e2f13a18e95cb63e2a618d0
Map-Sol <useris6631@gmail.com>
2026-04-19 21:39:05 +0530

    Update dtree.py

--- dtree.py
+++ dtree.py
@@ -3,14 +3,16 @@ from nltk.grammar import DependencyGrammar
 from nltk.parse import ProjectiveDependencyParser
 
 grammar = DependencyGrammar.fromstring("""
-'hit' -> 'boy' | 'ball'
+'hit' -> 'boy' | 'ball' | 'with'
 'boy' -> 'the'
 'ball' -> 'the'
+'with' -> 'bat'
+'bat' -> 'the'
 """)
 
 parser = ProjectiveDependencyParser(grammar)
 
-sentence = "the boy hit the ball".split()
+sentence = "the boy hit the ball with the bat".split()
 
 for tree in parser.parse(sentence):
     print(tree)
